082fdf74a60af271aec5d7f3648772c6ec1b9239
aVa <vitaly.ablovatsky@gmail.com>
2017-01-30 09:39:12 +0300

    30.01.2017 reload

--- web/classes/servlet/routing/SendPageRestaurants.java
+++ web/classes/servlet/routing/SendPageRestaurants.java
@@ -9,19 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/restaurant")
-public class SendPageRestaurant extends HttpServlet{
+@WebServlet(urlPatterns = "/restaurants")
+public class SendPageRestaurants extends HttpServlet{
 
-    private static final String RESTAURANTS_URL = "/WEB-INF/views/content/new_restaurant.jsp";
+    private static final String RESTAURANTS_URL = "/WEB-INF/views/content/new_restaurants.jsp";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //sdfdf
+        //sehfjeh
         HttpSession session = req.getSession();
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(RESTAURANTS_URL);
         session.setAttribute("routing", "views");
-        String id = req.getParameter("id");
-        session.setAttribute("restaurant", id);
         dispatcher.forward(req, resp);
     }
 
